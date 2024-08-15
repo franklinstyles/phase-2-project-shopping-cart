14cc74c23512d4aacf4ed96a40134c4600a7f189
Anthony <anthony.onyango@student.moringaschool.com>
2024-08-16 00:59:11 +0300

    added a search filter

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,38 +1,33 @@
-import React, { useState } from 'react'
-import { FaSearchengin } from "react-icons/fa6";
+import React, { useState } from 'react';
 import { MdOutlineShoppingCart } from "react-icons/md";
 import { Link } from 'react-router-dom';
 
+const Header = ({ onSearch, cartItems }) => {
 
-const Header = ({onSearch, cartItems, setCartItems}) => {
+  const handleSearchChange = (e) => {
+    onSearch(e.target.value);  
+  };
 
-  // const [cartItems, setCartItems] = useState([])
-
-  const handleBuy = (item) => {
-    setCartItems((prevItems) => [...prevItems, item])
-  }
   return (
     <div className='header'>
         <h1>FOOD PALACE</h1>
         <div className='search'>
             <input 
-            className='search'
-            id='search'
-            placeholder='Search...'
-            text='text'
+              className='search'
+              id='search'
+              placeholder='Search...'
+              type='text'
+              onChange={handleSearchChange} 
             />
         </div>
         <Link className='cart' to={"/cart"}>
-        <MdOutlineShoppingCart />
+          <MdOutlineShoppingCart />
           {cartItems.length > 0 && (
-          <span>{cartItems.length}</span>
-            
-
+            <span>{cartItems.length}</span>
           )}
-        
         </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
